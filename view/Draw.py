c040918ab60ffa3748580fb5bbafd6be968ac008
wikto <wiktor.goral@gmail.com>
2021-02-09 19:14:34 +0100

    Changed to use Layer class as input Added comments for most functions

--- view/Draw.py
+++ view/Draw.py
@@ -5,88 +5,109 @@ import numpy as np
 class Board:
     size = 0
     tile_size = 0
+    board_status = []
+    cell_types = []
 
-    def __init__(self, size=1000, tile_size=10):
+    def __init__(self, layers=None, tile_size=10):
+
+        self.size = layers[0].size
+        self.tile_size = tile_size
+        size_pixel = self.size * self.tile_size
+        self.board_status = np.zeros(shape=self.size)
+        for i in range(layers):
+            self.cell_types.append(layers[i].cell_types)
+
+        # Declaring window
         self.window = Tk()
-        self.window.geometry(str(size+140)+"x"+str(size))
+        self.window.geometry(str(size_pixel + 140) + "x" + str(size_pixel))
         self.window.title("Board")
-        self.canvas = Canvas(self.window, width=size, height=size)
 
-        self.window.rowconfigure(0, minsize=size, weight=1)
-        self.window.columnconfigure(1, minsize=size, weight=1)
+        # Splitting window into two columns for tooltip and canvas
+        self.window.rowconfigure(0, minsize=size_pixel, weight=1)
+        self.window.columnconfigure(1, minsize=size_pixel, weight=1)
 
-        self.buttons = Frame(self.window, height=size)
+        # Declaring canvas and tooltip
+        self.canvas = Canvas(self.window, width=size_pixel, height=size_pixel)
+        self.tooltip = Frame(self.window, height=size_pixel)
 
-        listbox_layer_label = Label(self.buttons, text="Select layer:")
-        listbox_layer = Listbox(self.buttons, font=('Times', 14), width=5, height=5)
-        listbox_layer.insert(1, "1")
-        listbox_layer.insert(2, "2")
-        listbox_layer.insert(2, "result")
-
-        listbox_cell_label = Label(self.buttons, text="Available cell types:")
-        listbox_cell = Listbox(self.buttons, font=('Times', 14), width=5, height=5)
-        listbox_cell.insert(1, "fire")
-        listbox_cell.insert(2, "water")
-        listbox_cell.itemconfig(0, {'bg': 'red'})
-        listbox_cell.itemconfig(1, {'bg': 'blue'})
-
-        btn_clear = Button(self.buttons, text="Clear layer")
-        btn_reset = Button(self.buttons, text="Reset all")
-
-        listbox_layer_label.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
-        listbox_layer.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
-        listbox_cell_label.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
-        listbox_cell.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
-        btn_clear.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
-        btn_reset.grid(row=5, column=0, sticky="ew", padx=5)
-
-        self.buttons.grid(row=0, column=0, sticky="ns")
+        # Laying out canvas and tooltip
+        self.tooltip.grid(row=0, column=0, sticky="ns")
         self.canvas.grid(row=0, column=1, sticky="nsew")
 
-        self.size = size
-        self.tile_size = tile_size
-        self.board_status = np.zeros(shape=(size // tile_size, size // tile_size))
-
+        # Initializing canvas
         self.initialize_board()
         self.canvas.bind("<B1-Motion>", self.click)
         self.canvas.bind("<Button-1>", self.click)
 
+        # Declaring layer list selection
+        listbox_layer_label = Label(self.tooltip, text="Select layer:")
+        self.listbox_layer = Listbox(self.tooltip, font=('Times', 14), width=5, height=5)
+        for i in range(layers):
+            self.listbox_layer.insert(i + 1, layers[i].name)
+        self.listbox_layer.activate(0)
+
+        # Declaring types of cells in layer list selection
+        listbox_cell_label = Label(self.tooltip, text="Available cell types:")
+        self.listbox_cell = Listbox(self.tooltip, font=('Times', 14), width=5, height=5)
+        for i in range(layers[0].cell_types):
+            # All cell types in layer
+            layer_types = layers[0].cell_types
+            self.listbox_cell.insert(i + 1, layer_types[i][0])
+            self.listbox_cell.itemconfig(i + 1, {'bg': layer_types[i][1]})
+        self.listbox_cell.activate(0)
+
+        # Buttons for clear and reset
+        btn_clear = Button(self.tooltip, text="Clear layer")
+        btn_reset = Button(self.tooltip, text="Reset all")
+
+        # Laying out buttons in tooltip
+        listbox_layer_label.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        self.listbox_layer.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
+        listbox_cell_label.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
+        self.listbox_cell.grid(row=3, column=0, sticky="ew", padx=5, pady=5)
+        btn_clear.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
+        btn_reset.grid(row=5, column=0, sticky="ew", padx=5)
+        self.mainloop()
+
     def mainloop(self):
         self.window.mainloop()
 
+    # Function to create netting for canvas
     def initialize_board(self):
-        tiles_number = self.size // self.tile_size
-        for i in range(tiles_number - 1):
+        for i in range(self.size - 1):
             self.canvas.create_line((i + 1) * self.tile_size, 0,
-                                    (i + 1) * self.tile_size, self.size)
+                                    (i + 1) * self.tile_size, self.size * self.tile_size)
 
-        for i in range(tiles_number - 1):
+        for i in range(self.size - 1):
             self.canvas.create_line(0, (i + 1) * self.tile_size,
-                                    self.size, (i + 1) * self.tile_size)
+                                    self.size * self.tile_size, (i + 1) * self.tile_size)
 
+    # Function to translate pixel position to grid coordinates
     def pixel_to_grid(self, pixel_position):
         pixel_position = np.array(pixel_position)
         return np.array(pixel_position // self.tile_size, dtype=int)
 
+    # Function to translate grid coordinates to pixel position
     def grid_to_pixel(self, grid_position):
         grid_position = np.array(grid_position, dtype=int)
         return self.tile_size * grid_position
 
+    # Function to color appropriate cell in grid
     def fill_cell(self, grid_position, color):
         pixel_position = self.grid_to_pixel(grid_position)
         self.canvas.create_rectangle(
             pixel_position[0], pixel_position[1],
             pixel_position[0] + self.tile_size, pixel_position[1] + self.tile_size,
-            fill=color
-        )
+            fill=color)
 
+    # On-Click function to change cell type
     def click(self, event):
+        # Get current mouse coordinates and translate them to grid coordinates
         pixel_position = [event.x, event.y]
         grid_position = self.pixel_to_grid(pixel_position)
-
-        self.fill_cell(grid_position)
-        self.board_status[grid_position[0]][grid_position[1]] = -1
-
-
-game_instance = Board(500, 50)
-game_instance.mainloop()
+        # Get current cell type
+        current_layer = self.listbox_layer.curselection()[0]
+        current_type = self.listbox_cell.curselection()[0]
+        # Fill rectangle with appropriate color and change cell's board status
+        self.fill_cell(grid_position, self.cell_types[current_layer][current_type][2])
+        self.board_status[grid_position[0]][grid_position[1]] = self.cell_types[current_layer][current_type][0]
